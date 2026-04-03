1a3db51f241f4128e81cbae53ab084f2af71c5a8
juliand924 <julianemanuel.drexler@uzh.ch>
2026-04-03 16:39:13 +0200

    updated session class to properly handle the id

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Session.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Session.java
@@ -14,8 +14,6 @@ import java.util.UUID;
 @Table(name = "session")
 public class Session implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(nullable = false, updatable = false)
