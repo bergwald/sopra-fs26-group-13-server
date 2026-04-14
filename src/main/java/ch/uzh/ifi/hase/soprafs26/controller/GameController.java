32231834debc56747af42c1d165c7d62d741c2c0
Patricia <patriciaandrea.kaeser@uzh.ch>
2026-04-14 11:18:12 +0200

    GameController: added authentication GameService: added Exception 404 Both: renamed function

--- src/main/java/ch/uzh/ifi/hase/soprafs26/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/GameController.java
@@ -8,6 +8,7 @@ import ch.uzh.ifi.hase.soprafs26.rest.dto.GameGetDTO;
 import ch.uzh.ifi.hase.soprafs26.rest.dto.GameDataGetDTO;
 import ch.uzh.ifi.hase.soprafs26.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs26.service.GameService;
+import ch.uzh.ifi.hase.soprafs26.service.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,18 +23,28 @@ import java.util.List;
 @RestController
 public class GameController {
 
+	private final UserService userService;
 	private final GameService gameService;
 
-	GameController(GameService gameService) {
+	GameController(GameService gameService, UserService userService) {
 		this.gameService = gameService;
+		this.userService = userService;
 	}
 
 	@GetMapping("/game_data")
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
-	public GameDataGetDTO GameDataGetDTO (@RequestBody GameGetDTO gameGetDTOin) {
+	public GameDataGetDTO getSessionRoundURL(@RequestBody GameGetDTO gameGetDTOin,
+		@RequestHeader(value = "Authorization", required = false) String authorizationHeader,
+		@RequestHeader(value = "userId", required = false) Long userId) {
+		
+		//authorize user logged in
+		String token = this.userService.extractBearerToken(authorizationHeader);
+		this.userService.getAuthorizedTargetUser(userId, token);
+
+		//find game_data info and return propper obj
 		Game_data input = DTOMapper.INSTANCE.convertGameGetDTOToEntity(gameGetDTOin);
-		Game_data output = gameService.getGameData(input);
+		Game_data output = gameService.getSessionRoundData(input);
 		return DTOMapper.INSTANCE.convertEntityToGameDataGetDTO(output);
 	}
 }
