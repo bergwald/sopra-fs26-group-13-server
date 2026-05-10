d62ebb9b25369834bbf0da9578e537c7d832dfcf
juliand924 <julianemanuel.drexler@uzh.ch>
2026-05-10 19:08:49 +0200

    updated region bounding boxes

--- src/main/java/ch/uzh/ifi/hase/soprafs26/constant/GameRegions.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/constant/GameRegions.java
@@ -16,14 +16,15 @@ public class GameRegions {
                                         new SearchRegion("AlpsBoxNorth", 5.943604, 46.057976, 16.677246, 48.180734),
                                         new SearchRegion("AlpsBoxSouth", 5.185547, 43.620213, 7.558594, 46.111319))),
                         Map.entry("NewZealandAlps",
-                                        List.of(new SearchRegion("NewZealandAlps", 165.366211, -47.676238, 177.429199,
-                                                        -38.872305))),
+                                        List.of(new SearchRegion("NewZealandAlpsQueensTown", 166.931763, -46.000200,
+                                                        169.650879, -44.225486),
+                                                        new SearchRegion("NewZealandAlpsUp", 170.837402, -43.268105,
+                                                                        172.880859, -42.355545))),
                         Map.entry("Himalaya",
                                         List.of(new SearchRegion("HimalayaCentral", 66.401367, 26.155291, 103.754883,
                                                         44.495013))),
                         Map.entry("JapaneseAlps",
-                                        List.of(new SearchRegion("JapaneseAlps", 136.582031, 34.385028, 142.998047,
-                                                        41.214540))),
+                                        List.of(new SearchRegion("JapaneseAlps", 137.362061,35.304546,139.394531,36.923664))),
                         Map.entry("Andes", List
                                         .of(new SearchRegion("Andes", -80.507813, -55.749307, -66.093750, 12.726004))));
 
